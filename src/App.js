5c06799d82397e0262996ec89a7bd3f4f3bee8bf
blazete <shreyansh7309992028@gmail.com>
2023-03-06 21:24:42 +0530

    Frst Commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/jsx-no-comment-textnodes */
+
+import './App.css';
+import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
+import { TransactionsList } from './components/TransactionsList';
+import { AddTransaction } from './components/AddTransaction';
+
+import { GlobalProvider } from './context/GlobalState';
+function App() {
+  return (
+    <GlobalProvider >
+      <Header />
+      <div className='container'>
+        <Balance />
+        <IncomeExpenses />
+        <TransactionsList />
+        <AddTransaction/>
+    </div>
+    </GlobalProvider>
+  );
+}
+
+export default App;
